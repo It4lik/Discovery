fbba6b21231ec54335854b34b929ec027625f983
it4 <it4@ntymail.com>
2017-03-09 17:35:41 +0100

    comments everywhere

--- Subnet.cs
+++ Subnet.cs
@@ -7,13 +7,13 @@ namespace discovery
     {
         // Class used to make IPv4 operations
         // This is gonna be exploded into an interface and a "Subnet" class that we can instantiate with one subnet (CIDR format) 
-        private string _CIDRAddress;
-        private  string _networkIP;
-        private int _maskCIDR; // like "24"
-        private string _netmask; // like "1111111111...100000..0000"
-        private string _firstFreeIP; // binary WITH dots
-        private string _lastFreeIP; // binary WITH dots
-        private string _broadcastIP; // binary WITH dots
+        private string _CIDRAddress; // Subnet address, CIDR format. Ex : "192.168.1.0/24"
+        private  string _networkIP; // Only subnet address. Ex : "192.168.1.0"
+        private int _maskCIDR; // Only mask, CIDR format. Ex : 24
+        private string _netmask; // Only mask, binary format, NO DOTS, 32 chars. Ex : "11111111111111111100000000000000"
+        private string _firstFreeIP; // First free IP address in subnet, binary, WITH DOTS, ends with 1. Ex : "11010111.11010011.11011000.0000001". 
+        private string _lastFreeIP; // Last free IP address in subnet, binary, WITH DOTS, ends with 0. Ex : "11010111.11010011.11011000.11111110".
+        private string _broadcastIP; // Last IP address of subnet, binary, WITH DOTS, ends with 1. Ex : "11010111.11010011.11011000.11111111".
 
         
         public Subnet(string addr) {
@@ -23,15 +23,16 @@ namespace discovery
                 _maskCIDR = System.Convert.ToInt32(_CIDRAddress.Split("/")[1]);
                 getNetmask(_networkIP, _maskCIDR);
                 getFirstAndLastIP(_networkIP, _maskCIDR);
-
             }
         }
         public bool verifyAddressCIDR(string CIDRAddress) {
-            Regex CIDRRegex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/(([0-9])|([12][0-9])|(3[0-2]))$");
+            // Used to verify that a string is a subnet IPv4 address formatted in CIDR (as in "192.168.1.0/24)
+            Regex CIDRRegex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/(([1-9])|([12][0-9])|(3[0-2]))$");
             return CIDRRegex.IsMatch(CIDRAddress);
         }
 
         private void getNetmask(string netIP, int maskCIDR) {
+            // Get netmask : iterate on subnet IPv4 address ||| from 0 to maskCIDR and fill with 1 ||| from maskCIDR to 32 (bits number in an IPv4 address) and fill with 0
             for (int i = 0; i < maskCIDR; i++)
             {
                 _netmask = System.String.Concat(_netmask, "1");
