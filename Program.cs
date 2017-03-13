a056b3c74f032ce08b0b6b3805a97b01b4692a0d
it4 <it4@bob.com>
2017-03-13 23:55:47 +0100

    explicit main

--- Program.cs
+++ Program.cs
@@ -9,28 +9,18 @@ namespace discovery
     class Program
     {
         static void Main(string[] args)
-        {            
-            /*Console.WriteLine("Starting...");
-            Stocker test = new Stocker("172.17.0.2", 6379);
-            Console.WriteLine(test.GetHost());
-
-            test.Write("yap", "666");
-            Console.WriteLine(test.Read("yap"));*/
-
-            // Console.WriteLine(Check.TCPTestHost("192.168.1.94", 8888));
-            //Check.CustomTest("192.168.1.94", 8788, "C:\\Windows\\System32\\bash.exe", "-c \"qls\"");
-
-            /*Subnet yop = new Subnet("192.168.1.0/24");
-            Scan.TCPScan(yop.getAllIPsInSubnet(), 9999);*/
-            
-            
+        {                
+            // Subnet on which do the tests
             Subnet LAN = new Subnet("192.168.1.0/24");
+            // Redis connection to use
             Stocker redis = new Stocker("127.0.0.1", 6379);
+            // Action to do if a host is up
             HostAction actionIfUp = new HostAction(HostAction.ActionType.SSHExec, "192.168.1.94", 2222, "john", "dbc", "echo \"<HOST> became UP at <TIME>\"  >> ZALU");
+            // Action to do if a host is down
             HostAction actionIfDown = new HostAction(HostAction.ActionType.SSHExec, "192.168.1.94", 2222, "john", "dbc", "echo \"<HOST> became DOWN at <TIME>\"  >> ZALU");
+            // Object that issue the discovery
             Discover disco = new Discover(Discover.CheckType.tcp, LAN, 6379, actionIfUp, actionIfDown, redis, "LAN");
             disco.startDiscovery();
-
         }
     }
 }
