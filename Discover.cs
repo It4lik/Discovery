e285972dff484ab0a3a4624352ac71dd7cefafc0
it4 <it4@bob.com>
2017-03-13 17:57:46 +0100

    Discover : thread shit

--- Discover.cs
+++ Discover.cs
@@ -14,38 +14,42 @@ namespace discovery
         private CheckType _checkType; // Check to use on subnet's hosts.
         private Subnet _targetedSubnet; // Subnet on which issue checks. 
         public Discover(CheckType checkType, Subnet targetedSubnet) {
+            // Initalize object properties
             _checkType = checkType;
             _targetedSubnet = targetedSubnet;
             _cts = new CancellationTokenSource();
-            initializeThread(_checkType);
+            // Initialize the _discovery thread with targeted check and subnet (checkType, targetedSubnet)
+            initializeDiscoveryThread(_checkType, _targetedSubnet, _cts, _discovery);
         }
 
-        public void startDiscovery() {
+        public void startDiscovery(Thread discovery) {
             // Used to start the thread that issue scans
-            _discovery.Start();
+            discovery.Start();
+            Console.WriteLine("Thread started."); // Debug console output
         }
 
-        public void stopDiscovery() {
+        public void stopDiscovery(CancellationTokenSource cts) {
             // Used to stop the thread tha issue scans (and cleanup afterwards)
-            _cts.Cancel();
-            _cts.Dispose();
+            Console.WriteLine("STOP CATCHED ! Aborting the thread."); // Debug console output
+            cts.Cancel();
+            cts.Dispose();
+            Console.WriteLine("Thread aborted."); // Debug console output
         }
-        private void initializeThread(CheckType checkType) {
+        private void initializeDiscoveryThread(CheckType checkType, Subnet targetedSubnet, CancellationTokenSource cts, Thread discovery) {
             switch (checkType) {
                 // _cts.Token is always sent : it is used to stop threads
                 case CheckType.tcp:
                     // Instanciate the thread with discoveryTcp method.
-                    _discovery = new Thread(delegate() {
-                        discoveryTcp(_targetedSubnet, _cts.Token);
+                    discovery = new Thread(delegate() {
+                        discoveryTcp(targetedSubnet, cts.Token);
                     });
                     break;
                 
                 default:
                     // Default is TCP check. See case CheckType.tcp:
-                    _discovery = new Thread(delegate() {
-                        discoveryTcp(_targetedSubnet, _cts.Token);
+                    discovery = new Thread(delegate() {
+                        discoveryTcp(_targetedSubnet, cts.Token);
                     });
-
                     break;
             }
         }
@@ -56,11 +60,15 @@ namespace discovery
 
         private void discoveryTcp(Subnet targetedSubnet, CancellationToken cancelToken) {
             // thread that open TCP connections
+
+            //  ####  TEMP CODE  ####  //
+
             while (true) {
 
                 if(cancelToken.IsCancellationRequested) {
                     // If stopDiscovery() was called. That means that someone wants to stop the discovery, and thus, this thread. 
                     // Cleanup and exit the while loop.  
+                    Console.WriteLine("Cancel method called. Cancellation requested."); // Debug console output
                     return;
                 }
 
@@ -73,10 +81,6 @@ namespace discovery
                     // if no, store the host in redis
                 // retrieve list of existing hosts in Redis
                 // if a host in that list is NOT in the string list of alive hosts : delete it from redis 
-
-                // TEMP CODE //
-
-
             }
         }
     }
