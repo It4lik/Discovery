053807340d6ce06a9a03a756abee00cd4ad259b7
it4 <it4@ntymail.com>
2017-03-15 23:17:08 +0100

    updated readme

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using StackExchange.Redis;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace discovery
 {
@@ -11,22 +10,17 @@ namespace discovery
     {
         static void Main(string[] args)
         {                
-
-            Regex CIDRRegex = new Regex(@"^(([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.)(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){2}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/(([1-9])|([12][0-9])|(3[0-2]))$");
-            Console.WriteLine(CIDRRegex.IsMatch("172.17.0.0/24"));
             // Subnet on which do the tests
             Subnet LAN = new Subnet("172.17.0.0/24");
-
             // Redis connection to use
-            Stocker redis = new Stocker("127.0.0.1", 6379);
+            Stocker redis = new Stocker("177.177.1.10", 6379);
             // Action to do if a host is up
-            HostAction actionIfUp = new HostAction(HostAction.ActionType.SSHExec, "127.0.0.1", 4444, "john", "", "echo \"<HOST> became UP at <TIME>\"  >> ZALU");
+            HostAction actionIfUp = new HostAction(HostAction.ActionType.SSHExec, "177.177.1.21", 22, "john", "", "echo \"<HOST> became UP at <TIME>\"  >> logfile");
             // Action to do if a host is down
-            HostAction actionIfDown = new HostAction(HostAction.ActionType.SSHExec, "127.0.0.1", 4444, "john", "", "echo \"<HOST> became DOWN at <TIME>\"  >> ZALU");
+            HostAction actionIfDown = new HostAction(HostAction.ActionType.SSHExec, "177.177.1.21", 22, "john", "", "echo \"<HOST> became DOWN at <TIME>\"  >> logfile");
             // Object that issue the discovery
-            Discover disco = new Discover("LAN", Discover.CheckType.tcp, LAN, 6379, actionIfUp, actionIfDown, redis);
+            Discover disco = new Discover("CT", Discover.CheckType.tcp, LAN, 6379, actionIfUp, actionIfDown, redis);
             disco.StartDiscovery();
-
         }
     }
 }
