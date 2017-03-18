c9618643a326d7b55618c9be8a3d4f05623b3527
it4 <it4@ntymail.com>
2017-03-18 22:00:32 +0100

    updated dockerized version

--- Program.cs
+++ Program.cs
@@ -12,15 +12,15 @@ namespace discovery
         {   
                 
             // Subnet on which do the tests
-            Subnet LAN = new Subnet("192.168.1.0/20");
+            Subnet LAN = new Subnet("177.177.1.0/20");
             // Redis connection to use
-            Stocker redis = new Stocker("192.168.1.94", 6379);
+            Stocker redis = new Stocker("177.177.1.10", 6379);
             // Action to do if a host is up
-            HostAction actionIfUp = new HostAction(HostAction.ActionType.SSHExec, "192.168.1.94", 2222, "john", "dbc", "echo \"<HOST> became UP at <TIME>\"  >> ZALU");
+            HostAction actionIfUp = new HostAction(HostAction.ActionType.SSHExec, "177.177.1.21", 2222, "john", "dbc", "echo \"<HOST> became UP at <TIME>\"  >> ZALU");
             // Action to do if a host is down
-            HostAction actionIfDown = new HostAction(HostAction.ActionType.SSHExec, "192.168.1.94", 2222, "john", "dbc", "echo \"<HOST> became DOWN at <TIME>\"  >> ZALU");
+            HostAction actionIfDown = new HostAction(HostAction.ActionType.SSHExec, "177.177.1.21", 2222, "john", "dbc", "echo \"<HOST> became DOWN at <TIME>\"  >> ZALU");
             // Object that issue the discovery
-            Discover disco = new Discover(Discover.CheckType.tcp, LAN, 443, actionIfUp, actionIfDown, redis, "LAN", 6);
+            Discover disco = new Discover(Discover.CheckType.tcp, LAN, 6379, actionIfUp, actionIfDown, redis, "CTNET", 6);
             disco.startDiscovery();
 
         }
